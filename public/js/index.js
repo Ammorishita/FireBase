542d81c18528be3388d48b1f65c95e4566c83b4a
Ammorishita <ammorishita@gmail.com>
2017-09-04 14:17:19 -0700

    Fix projects and skills section

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,26 @@
 let navigation = {
 	init: function() {
+		this.testing();
+	},
+	testing: function() {
+		let misc = document.querySelector('.misc');
+		let buttons = document.querySelectorAll('.button');
+		misc.addEventListener('click', highlight);
+		let chains = document.querySelectorAll('.chain');
+		function highlight(e) {
+			let current = e.target;
+			if(current.classList.contains('button-active')) {
+
+			} else {
+				buttons.forEach( e => {
+					e.classList.remove('button-active');
+				})
+				current.classList.add('button-active');
+				chains.forEach( e => {
+					e.classList.toggle('active');
+				})
+			}
+		}
 	}	
 }
 navigation.init();
